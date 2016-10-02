2daa8e82727f226f9c751d2db5e252adbc305929
Tatsuya Kawano <tatsuya@hibaridb.org>
2016-10-02 13:56:08 +0800

    Update the main.rs to print the number of key-values processed

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
 
-extern crate hibari_brick_rs;
-
-use hibari_brick_rs as brick;
+extern crate hibari_brick_rs as brick;
 
 use std::thread;
 use std::sync::Arc;
@@ -25,8 +23,8 @@ fn main() {
         assert_eq!(value.to_vec(), val.unwrap());
     };
 
-    do_ops(brick_id, NUM_THREADS, NUM_KEYS_PER_THREAD, put_op, "put");
-    do_ops(brick_id, NUM_THREADS, NUM_KEYS_PER_THREAD, get_op, "get");
+    do_ops(brick_id, NUM_THREADS, NUM_KEYS_PER_THREAD, "put", put_op);
+    do_ops(brick_id, NUM_THREADS, NUM_KEYS_PER_THREAD, "get", get_op);
 
     brick::shutdown();
 
@@ -36,11 +34,10 @@ fn main() {
 fn do_ops<F>(brick_id: brick::BrickId,
              num_threads: u32,
              num_keys_per_thread: u32,
-             op: F,
-             op_name: &str)
+             op_name: &str,
+             op: F)
     where F: Fn(brick::BrickId, &str, &[u8], &[u8]) + Send + Sync + 'static
 {
-
     let op = Arc::new(op);
 
     let handles: Vec<_> = (0..num_threads)
@@ -50,20 +47,24 @@ fn do_ops<F>(brick_id: brick::BrickId,
             let my_op_name = op_name.to_owned();
 
             thread::spawn(move || {
-                println!("Thread {} started for {} ops.", n, my_op_name);
+                println!("Thread {} started for {} operations.", n, my_op_name);
 
                 let start = n * num_keys_per_thread;
                 let end = start + num_keys_per_thread;
                 let mut key;
                 let mut value;
+                let mut count = 0;
 
                 for i in start..end {
                     key = format!("key{:010}", i);
                     value = format!("value{:010}", i);
                     my_op(brick_id, &key, key.as_bytes(), value.as_bytes());
+                    count += 1;
                 }
 
-                println!("Thread {} ended.", n);
+                println!("Thread {} ended. (Performed {} {} operations.", n, count, my_op_name);
+
+                assert_eq!(num_keys_per_thread, count);
             })
         })
         .collect();
