caaf16e512829a9aab7514a7ffc32833698f2017
Tatsuya Kawano <tatsuya@hibaridb.org>
2017-01-31 00:06:48 +0800

    Update copyright year (2017)

--- src/hlog/hunk.rs
+++ src/hlog/hunk.rs
@@ -1,5 +1,5 @@
 // ----------------------------------------------------------------------
-//  Copyright (c) 2016 Hibari developers. All rights reserved.
+//  Copyright (c) 2016-2017 Hibari developers. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -12,9 +12,6 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
-//
-//  File    : brick_hlog_hunk.rs
-//  Purpose : log hunk used by brick_hlog.
 // ----------------------------------------------------------------------
 
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+// ----------------------------------------------------------------------
+//  Copyright (c) 2016-2017 Hibari developers. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// ----------------------------------------------------------------------
+
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
 
@@ -19,6 +35,11 @@ use rocksdb::{DB, Options};
 use rustc_serialize::{Encodable, Decodable};
 
 use std::io;
+
+//
+// cargo rustc --lib -- -Z unstable-options --unpretty=''hir,typed'' (zsh)
+// cargo rustc --lib -- -Z unstable-options --unpretty=hir,typed     (bash)
+// cargo rustc --lib -- -Z unstable-options --unpretty=mir
 
 pub mod hlog {
     pub mod hunk;
